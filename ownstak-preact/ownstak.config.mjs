9b7d7e9b186f78e3b5ccda3cade25568b84da3ae
OwnStak Team <team@ownstak.com>
2025-07-31 16:55:20 +0200

    Added ownstak-preact

---
+++ ownstak-preact/ownstak.config.mjs
@@ -0,0 +1,14 @@
+import { Config } from 'ownstak';
+/**
+ * This is the default config for your OwnStak project.
+ * Use it to customize the OwnStak project behavior
+ * and commit it to your project source code.
+ * @see https://docs.ownstak.com/cli/configuration
+ */
+export default new Config()
+    .setFramework('static')
+    .setBuildCommand('npx vite build')
+    .setDevCommand('npx vite dev')
+    .setDefaultStatus(200)
+    .setDefaultFile("index.html")
+    .includeAsset("./dist/", "./")
